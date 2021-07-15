a0252034890d2cfd5809e021c3b164cef26e433c
SeikyouDev <seikyoudev@gmail.com>
2021-07-15 16:26:57 -0500

    website update

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,31 @@
-// const themeToggle = document.querySelector('.theme-toggle')
-// const lightButton = document.querySelector('.light-btn')
-// const darkButton = document.querySelector('.dark-btn')
+const themeToggle = document.querySelector('.theme-toggle')
+const lightButton = document.querySelector('.light-btn')
+const darkButton = document.querySelector('.dark-btn')
 
-// themeToggle.addEventListener('click', () => {
-//   document.body.classList.contains('light-theme') ? enableDarkMode() : enableLightMode()
-// })
+themeToggle.addEventListener('click', () => {
+  document.body.classList.contains('light-theme') ? enableDarkMode() : enableLightMode()
+})
 
-// lightButton.addEventListener('click', () => {
-//   enableLightMode()
-// })
+function enableLightMode() {
+  document.body.classList.remove('dark-theme')
+  document.body.classList.add('light-theme')
+  lightButton.style.display = 'none'
+  darkButton.style.display = 'block'
+}
 
-// darkButton.addEventListener('click', () => {
-//   enableDarkMode()
-// })
+function enableDarkMode() {
+  document.body.classList.remove('light-theme')
+  document.body.classList.add('dark-theme')
+  lightButton.style.display = 'block'
+  darkButton.style.display = 'none'
+}
 
-// function enableDarkMode() {
-//   document.body.classList.remove('light-theme')
-//   document.body.classList.add('dark-theme')
-//   themeToggle.setAttribute('aria-label', 'Switch to light theme')
-// }
+function setThemePreference() {
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    enableDarkMode()
+    return
+  }
+  enableLightMode()
+}
 
-// function enableLightMode() {
-//   document.body.classList.remove('dark-theme')
-//   document.body.classList.add('light-theme')
-//   themeToggle.setAttribute('aria-label', 'Switch to dark theme')
-// }
-
-// function setThemePreference() {
-//   if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-//     enableDarkMode()
-//     return
-//   }
-//   enableLightMode()
-// }
-
-// document.onload = setThemePreference()
+document.onload = setThemePreference()
