266e8254d9d9e6dc1d82a209d388665c7ba42ddd
SeikyouDev <seikyoudev@gmail.com>
2021-07-18 22:45:12 -0500

    website update

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,27 @@ let menuToggle = document.querySelector('.mobile-header_button')
 let menuLinks = document.querySelectorAll('.mobile-header_menu-link')
 let topButton = document.querySelector('.top-button')
 
+// Include Smooth Scroll JS
+var scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 750,
+  speedAsDuration: true,
+  updateURL: false,
+  topOnEmptyHash: true,
+})
+
+//Include Menuspy JS
+// var elm = document.querySelector('#menuspy') // Desktop Menu
+// var ms = new MenuSpy(elm, {
+//   activeClass: 'current',
+//   enableLocationHash: false,
+// })
+
+// var elm2 = document.querySelector('#mobile-menuspy') // Mobile Menu
+// var ms2 = new MenuSpy(elm2, {
+//   activeClass: 'current',
+//   enableLocationHash: false,
+// })
+
 // Open / Close (Toggle) the mobile menu when you click the menu toggle button
 menuToggle.addEventListener('click', function () {
   menuToggle.classList.toggle('is-active')
