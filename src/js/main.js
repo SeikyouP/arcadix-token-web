e0fd054e1fb48e0b48333838226264fc4f44c2ed
SeikyouDev <seikyoudev@gmail.com>
2021-07-20 22:38:59 -0500

    website update

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 //  Declare Variables
+let body = document.querySelector('body')
 let header = document.querySelector('.header')
 let mobileHeader = document.querySelector('.mobile-header')
 let menuToggle = document.querySelector('.mobile-header_button')
@@ -41,13 +42,33 @@ window.addEventListener('scroll', () => {
 menuToggle.addEventListener('click', function () {
   menuToggle.classList.toggle('is-active')
   mobileHeader.classList.toggle('mobile-header_open')
+  body.classList.toggle('disable-scroll')
+
+  // Change mobile header background color when you click the menu toggle button
+  if (window.pageYOffset === 0) {
+    mobileHeader.classList.toggle('active')
+  }
+  // Otherwise, do nothing
+  else {
+    return
+  }
 })
 
 // Close the moblie menu when you click on any mobile link
 for (i = 0; i < menuLinks.length; i++) {
-  menuLinks[i].addEventListener('click', function () {
+  menuLinks[i].addEventListener('click', function (event) {
     menuToggle.classList.remove('is-active')
     mobileHeader.classList.remove('mobile-header_open')
+    body.classList.remove('disable-scroll')
+
+    // Change mobile header background color when you click the menu toggle button
+    if (window.pageYOffset === 0 && event.target.id === 'home') {
+      mobileHeader.classList.remove('active')
+    }
+    // Otherwise, do nothing
+    else if (window.pageYOffset === 0) {
+      return
+    }
   })
 }
 
