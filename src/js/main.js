1dd16c6a40252011c723cdaac66dcee20fc1f5b3
SeikyouDev <seikyoudev@gmail.com>
2021-07-24 16:20:33 -0500

    website update

--- src/js/main.js
+++ src/js/main.js
@@ -35,18 +35,20 @@ for (let i = 0; i < question.length; i++) {
 }
 
 for (let i = 0; i < plusMinusButton.length; i++) {
-  plusMinusButton[i].addEventListener('touchend', () => {
+  plusMinusButton[i].addEventListener('touchend', (event) => {
     plusButton[i].classList.toggle('content_plus-icon--active')
     minusButton[i].classList.toggle('content_minus-icon--active')
     answer[i].classList.toggle('content_answer--open')
+    event.preventDefault()
   })
 }
 
 for (let i = 0; i < question.length; i++) {
-  question[i].addEventListener('touchend', () => {
+  question[i].addEventListener('touchend', (event) => {
     plusButton[i].classList.toggle('content_plus-icon--active')
     minusButton[i].classList.toggle('content_minus-icon--active')
     answer[i].classList.toggle('content_answer--open')
+    event.preventDefault()
   })
 }
 
